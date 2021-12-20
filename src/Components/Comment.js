e9453f88ad239d9fdedd511d50d40bbfc087e48f
Tirth <tirthcoder@gmail.com>
2021-12-20 22:14:20 +0530

    Comment functionality added

---
+++ src/Components/Comment.js
@@ -0,0 +1,38 @@
+import React, {useState,useEffect} from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Avatar from '@mui/material/Avatar';
+import {database} from '../firebase';
+
+function Comment({postData}) {
+    const [comments, setComments] = useState(null);
+    
+    useEffect(async() => {
+        let arr = [];
+        for(let i = 0; i < postData.comments.length; i++){
+            let data = await database.comments.doc(postData.comments[i]).get();
+            arr.push(data.data());
+        }
+
+        setComments(arr);
+    }, [postData])
+
+    return (
+        <div>
+            {
+                comments==null? <CircularProgress/> :
+                <>
+                {
+                    comments.map((comment) => (
+                        <div style={{display:'flex'}}>
+                            <Avatar src={comment.userProfile}/>
+                            <p>&nbsp;&nbsp; <span style={{fontWeight:'bold'}}>{comment.userName}</span>&nbsp;&nbsp; {comment.text}</p>
+                        </div>
+                    ))
+                }
+                </>
+            }
+        </div>
+    )
+}
+
+export default Comment;
--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,52 +1,117 @@
-import { CircularProgress } from '@mui/material';
-import React, {useState, useEffect} from 'react';
-import { database } from '../firebase';
-import Video from './Video';
-import './Posts.css';
-import Avatar from '@mui/material/Avatar';
-import Like from './Like';
+import { CircularProgress } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { database } from "../firebase";
+import Video from "./Video";
+import "./Posts.css";
+import Avatar from "@mui/material/Avatar";
+import Like from "./Like";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import Card from "@mui/material/Card";
+import Typography from "@mui/material/Typography";
+import Dialog from "@mui/material/Dialog";
+import AddComment from "./AddComment";
+import ShowLike from "./ShowLike";
+import Comment from "./Comment";
 
-function Posts({userData}) {
+function Posts({ userData }) {
+  const [posts, setPosts] = useState(null);
+  const [open, setOpen] = React.useState(false);
 
-    const [posts, setPosts] = useState(null);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    // Getting all the videos in desc order (latest one at top)
-    useEffect(() => {
-        let postsArr = [];
-        const unsub = database.posts.orderBy('createdAt', 'desc').onSnapshot((querySnapshot) => {
-            postsArr = [];
-            querySnapshot.forEach((doc) => {
-                let data = {...doc.data(), postId: doc.id}
-                postsArr.push(data);
-            })
-            setPosts(postsArr);
-        }) 
-        return unsub;
-    }, [])
-    
-    return (
-        <div>
-            {   
-                posts == null || userData == null ? <CircularProgress /> :
-                <div className="video-container">
-                    {
-                        posts.map((post,index)=>(
-                            <React.Fragment key={index}>
-                                <div className="videos">
-                                    <Video src={post.postUrl} />
-                                    <div className="fa">
-                                        <Avatar src={post.userProfile} />
-                                        <h4>{post.userName}</h4>
-                                    </div>
-                                    <Like userData={userData} postData={post} />
-                                </div>
-                            </React.Fragment>
-                        ))
-                    }
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // Getting all the videos in desc order (latest one at top)
+  useEffect(() => {
+    let postsArr = [];
+    const unsub = database.posts
+      .orderBy("createdAt", "desc")
+      .onSnapshot((querySnapshot) => {
+        postsArr = [];
+        querySnapshot.forEach((doc) => {
+          let data = { ...doc.data(), postId: doc.id };
+          postsArr.push(data);
+        });
+        setPosts(postsArr);
+      });
+    return unsub;
+  }, []);
+
+  return (
+    <div>
+      {posts == null || userData == null ? (
+        <CircularProgress />
+      ) : (
+        <div className="video-container">
+          {posts.map((post, index) => (
+            <React.Fragment key={index}>
+              <div className="videos">
+                <Video src={post.postUrl} />
+                <div className="fa">
+                  <Avatar src={post.userProfile} />
+                  <h4>{post.userName}</h4>
                 </div>
-            }
+                <Like userData={userData} postData={post} />
+                <ChatBubbleIcon className="chat" onClick={handleClickOpen} />
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+                  fullWidth={true}
+                  maxWidth="lg"
+                >
+                  <div className="modal-container">
+                    <div className="video-modal">
+                      <video autoPlay="true" muted="muted" controls>
+                        <source src={post.postUrl} />
+                      </video>
+                    </div>
+                    <div className="comment-modal">
+                    <Card className="card1">
+                        <Comment postData={post} />
+                    </Card>
+                      <Card variant="outlined" className="card2">
+                        <Typography style={{ padding: "0.4rem" }}>
+                          {post.likes.length == 0
+                            ? "Liked by nobody"
+                            : `Liked by ${post.likes.length} users`}
+                        </Typography>
+                        <div>
+                          <ShowLike
+                            postData={post}
+                            userData={userData}
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                            }}
+                          />
+                          <AddComment
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                            }}
+                            userData={userData}
+                            postData={post}
+                          />
+                        </div>
+                      </Card>
+                    </div>
+                  </div>
+                </Dialog>
+              </div>
+            </React.Fragment>
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 export default Posts;
