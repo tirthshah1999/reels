04bc264501339b46c6ab3bf359d64b234ea83686
Tirth <tirthcoder@gmail.com>
2021-12-23 20:26:56 +0530

    update

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,8 @@ import 'firebase/compat/auth';
 import 'firebase/compat/storage';       // to store large size like(file, videos, images)
 import 'firebase/compat/firestore';    // to store values (key-value format)
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAYvhOVEN_y0hNed2FdE2Fb4ZP05AzIOdo",
-  authDomain: "reels-project-c3daf.firebaseapp.com",
-  projectId: "reels-project-c3daf",
-  storageBucket: "reels-project-c3daf.appspot.com",
-  messagingSenderId: "664386584883",
-  appId: "1:664386584883:web:bcf96bda9e76a3dab5fe46"
+  // Your web app's Firebase configuration
 };
 
 // Initialize Firebase
